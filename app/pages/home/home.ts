9192667183defa77021f2f76fc7be6a540da51e7
VietAnhh <viet.le175@gmail.com>
2016-09-27 06:25:30 -0400

    firebase sdk

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,23 +1,29 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import {
-  Push,
-  PushToken
-} from '@ionic/cloud-angular';
+import {Component, NgZone} from '@angular/core';
+import {NavController, Events} from 'ionic-angular';
+declare var firebase:any;
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController, public push: Push) {
 
-  }
+  zone;
+  fbValue;
 
-  pushButton(){
-    this.push.rx.notification()
-      .subscribe((msg) => {
-        alert(msg.title + ': ' + msg.text);
+  constructor(public navCtrl: NavController, private events: Events) {
+    this.zone = new NgZone({enableLongStackTrace: false});
+    let valueChanged = firebase.database().ref();
+    valueChanged.on('value', (snapshot) => {
+      console.log(snapshot.val());
+
+      let obj = snapshot.val();
+
+      this.zone.run(() => {
+        this.fbValue = obj.show_me;
+        console.log(this.fbValue);
       });
+
+    });
   }
 
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import {
-  Push,
-  PushToken
-} from '@ionic/cloud-angular';
+// import {
+//   Push,
+//   PushToken
+// } from '@ionic/cloud-angular';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -16,16 +16,17 @@ export class TabsPage {
   public tab2Root: any;
   public tab3Root: any;
 
-  constructor(public push: Push) {
+  // constructor(public push: Push) {
+  constructor() {
     // this tells the tabs component which Pages
     // should be each tab's root Page
 
-
-    this.push.register().then((t: PushToken) => {
-      return this.push.saveToken(t);
-    }).then((t: PushToken) => {
-      console.log('Token saved:', t.token);
-    });
+    //
+    // this.push.register().then((t: PushToken) => {
+    //   return this.push.saveToken(t);
+    // }).then((t: PushToken) => {
+    //   console.log('Token saved:', t.token);
+    // });
 
     this.tab1Root = HomePage;
     this.tab2Root = AboutPage;
