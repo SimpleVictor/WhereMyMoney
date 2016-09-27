7b39f9ad5729c7a568ff145af0c7282a02ce5c72
Victor <viet.le175@gmail.com>
2016-09-26 21:36:32 -0400

    added push notifications

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
 
 @Component({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
-  
+  constructor(public navCtrl: NavController, public push: Push) {
+
   }
+
+  pushButton(){
+    this.push.rx.notification()
+      .subscribe((msg) => {
+        alert(msg.title + ': ' + msg.text);
+      });
+  }
+
 }
--- app/pages/tabs/tabs.ts
+++ app/pages/tabs/tabs.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { HomePage } from '../home/home';
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
+import {
+  Push,
+  PushToken
+} from '@ionic/cloud-angular';
 
 @Component({
   templateUrl: 'build/pages/tabs/tabs.html'
@@ -12,9 +16,17 @@ export class TabsPage {
   public tab2Root: any;
   public tab3Root: any;
 
-  constructor() {
+  constructor(public push: Push) {
     // this tells the tabs component which Pages
     // should be each tab's root Page
+
+
+    this.push.register().then((t: PushToken) => {
+      return this.push.saveToken(t);
+    }).then((t: PushToken) => {
+      console.log('Token saved:', t.token);
+    });
+
     this.tab1Root = HomePage;
     this.tab2Root = AboutPage;
     this.tab3Root = ContactPage;
